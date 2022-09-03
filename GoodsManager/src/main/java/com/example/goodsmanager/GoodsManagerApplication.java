d2764e0c251b81aafe6df874510fb68dfa8b45fc
w1_liamby <weiliang_bai@foxmail.com>
2022-09-03 20:16:12 +0800

    init

---
+++ GoodsManager/src/main/java/com/example/goodsmanager/GoodsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.goodsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsManagerApplication.class, args);
+    }
+
+}
---
+++ GoodsManager/src/test/java/com/example/goodsmanager/GoodsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
